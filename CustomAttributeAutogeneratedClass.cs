5a1e41f331b7515fa7b5c475e9ec37d83de8d151
Timothy Graupmann <tgraupmann@gmail.com>
2021-10-23 17:22:29 -0700

    Added generics for easy access

---
+++ CustomAttributeAutogeneratedClass.cs
@@ -0,0 +1,13 @@
+﻿namespace DynamicsEntityGenerator
+{
+    [System.AttributeUsage(System.AttributeTargets.Class)]
+    public class TableNameAttribute : System.Attribute
+    {
+        public string Name { get; }
+
+        public TableNameAttribute(string name)
+        {
+            this.Name = name;
+        }
+    }
+}
